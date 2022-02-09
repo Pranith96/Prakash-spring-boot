62e59c7c6e5c1592260f4e6c06e4e0b45d8939f3
Your Name <you@example.com>
2022-02-09 10:38:18 +0530

    initial commit with spring boot application

---
+++ student-application-prakash/src/main/java/com/student/StudentApplicationPrakashApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplicationPrakashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplicationPrakashApplication.class, args);
+	}
+
+}
